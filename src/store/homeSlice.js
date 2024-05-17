db6463b652f8f897f748a8cebafa1217373fe122
Richard Ochei <162317835+OxheiCodes@users.noreply.github.com>
2024-05-17 13:00:45 +0100

    Comments Added

--- src/store/homeSlice.js
+++ src/store/homeSlice.js
@@ -1,22 +1,28 @@
-import { createSlice } from '@reduxjs/toolkit'
+// Author: Richard Ochei (OxheiCodes)
 
+import { createSlice } from '@reduxjs/toolkit';
+
+// Create a Redux slice named 'home'
 export const homeSlice = createSlice({
   name: 'home',
-  initialState:{
-    url:{},
-    genres:{}
+  initialState: {
+    url: {}, // Initial state for the 'url' object
+    genres: {}, // Initial state for the 'genres' object
   },
   reducers: {
-    getApiConfiguration : (state,action)=>{
-        state.url = action.payload;
+    // Reducer: Updates the 'url' state with the payload provided
+    getApiConfiguration: (state, action) => {
+      state.url = action.payload;
     },
-    getGenres : (state,action)=>{
-        state.genres = action.payload;
-    }
+    // Reducer: Updates the 'genres' state with the payload provided
+    getGenres: (state, action) => {
+      state.genres = action.payload;
+    },
   },
-})
+});
 
-// Action creators are generated for each case reducer function
-export const { getApiConfiguration, getGenres } = homeSlice.actions
+// Action creators are automatically generated for each case reducer function
+export const { getApiConfiguration, getGenres } = homeSlice.actions;
 
-export default homeSlice.reducer
+// Export the reducer function
+export default homeSlice.reducer;
